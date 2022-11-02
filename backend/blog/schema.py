99a74dd8ebb70d04cedb081b98a20084af0c019e
Avivar101 <benjaminokezie.ob@gmail.com>
2022-11-02 12:02:01 +0100

    Update schema.py

--- backend/blog/schema.py
+++ backend/blog/schema.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
 from graphene_django import DjangoObjectType
+import graphene
 
 from blog import models
 
@@ -22,3 +23,30 @@ class PostType(DjangoObjectType):
 class TagType(DjangoObjectType):
     class Meta:
         model = models.Tag
+
+
+class Query(graphene.ObjectType):
+    all_post = graphene.List(PostType)
+    author_by_username = graphene.Field(AuthorType, username=graphene.String())
+    post_by_slug = graphene.Field(PostType, slug=graphene.String())
+    posts_by_author = graphene.List(PostType, username=graphene.String())
+    posts_by_tag = graphene.List(PostType, tag=graphene.String())
+
+    def resolve_all_posts(root, info):
+        return (
+            models.Post.objects.prefetch_related("tags")
+            .selected_related("author")
+            .all()
+        )
+
+    def resolve_author_by_username(root, info, username):
+        return ()
+
+    def resolve_post_by_slug(self):
+        pass
+
+    def resolve_posts_by_author(self):
+        pass
+
+    def resolve_posts_by_tag(self):
+        pass
