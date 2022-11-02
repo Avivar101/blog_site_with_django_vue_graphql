88f0e2c55fd8923542792c0019818761e78fa56a
Avivar101 <benjaminokezie.ob@gmail.com>
2022-11-02 09:59:04 +0100

    commit

--- backend/blog/models.py
+++ backend/blog/models.py
@@ -1,10 +1,21 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
 class Profile(models.Model):
-    user = models.OneToOneField
-    website = None
-    bio = None
+    user = models.OneToOneField(
+        User,
+        on_delete=models.PROTECT,
+    )
+    website = models.URLField(blank=True)
+    bio = models.CharField(max_length=240, blank=True)
 
     def __str__(self):
         return self.user.get_username()
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
